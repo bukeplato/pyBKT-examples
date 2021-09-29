d85ebc5612d77e8f1573cc0313e4df9379f27265
Qian <book1925@gmail.com>
2021-09-29 21:46:11 +0800

    加了主函数入口，修改论文排版

--- KtIdem.py
+++ KtIdem.py
@@ -1,16 +1,26 @@
 import sys
-sys.path.append('../')
 import numpy as np
 from pyBKT.models import Model
-np.seterr(divide='ignore', invalid='ignore')
 
-skills = ["Percent Of", "Addition and Subtraction Integers", "Conversion of Fraction Decimals Percents", "Volume Rectangular Prism", "Venn Diagram", "Equation Solving Two or Fewer Steps", "Volume Cylinder", "Multiplication and Division Integers", "Area Rectangle", "Addition and Subtraction Fractions", ]
+sys.path.append('../')
 
-model = Model(seed = 0, num_fits = 20)
-print("BKT")
-#print(model.crossvalidate(data_path = "data/as.csv", skills = skills, metric = "auc"))
-print(model.crossvalidate(data_path = "data/as.csv", skills = skills, metric = "rmse"))
-print()
-print("KT-IDEM")
-#print(model.crossvalidate(data_path = "data/as.csv", skills = skills, multigs = True, metric = "auc"))
-print(model.crossvalidate(data_path = "data/as.csv", skills = skills, multigs = True, metric = "rmse"))
+
+def main():
+    np.seterr(divide='ignore', invalid='ignore')
+
+    skills = ["Percent Of", "Addition and Subtraction Integers", "Conversion of Fraction Decimals Percents",
+              "Volume Rectangular Prism", "Venn Diagram", "Equation Solving Two or Fewer Steps", "Volume Cylinder",
+              "Multiplication and Division Integers", "Area Rectangle", "Addition and Subtraction Fractions", ]
+
+    model = Model(seed=0, num_fits=20)
+    print("BKT")
+    # print(model.crossvalidate(data_path = "data/as.csv", skills = skills, metric = "auc"))
+    print(model.crossvalidate(data_path="data/as.csv", skills=skills, metric="rmse"))
+    print()
+    print("KT-IDEM")
+    # print(model.crossvalidate(data_path = "data/as.csv", skills = skills, multigs = True, metric = "auc"))
+    print(model.crossvalidate(data_path="data/as.csv", skills=skills, multigs=True, metric="rmse"))
+
+
+if __name__ == '__main__':
+    sys.exit(main())
