2141cd70282f2b5a38d1e823167b80cfb41a6b0c
Qian <book1925@gmail.com>
2021-10-19 15:26:35 +0800

    去掉BasicForgets.py的基础模型训练

--- BasicForgets.py
+++ BasicForgets.py
@@ -1,15 +1,16 @@
 import sys
 import numpy as np
+np.seterr(divide='ignore', invalid='ignore')
 from pyBKT.models import Model
 
 sys.path.append('../')
 
 
 def main():
-    np.seterr(divide='ignore', invalid='ignore')
-    model = Model(seed=0, num_fits=20)
-    model.fit(data_path="data/builder_train_preprocessed.csv")
-    print("Standard BKT:", model.evaluate(data_path="data/builder_test_preprocessed.csv", metric="auc"))
+
+    # model = Model(seed=0, num_fits=20)
+    # model.fit(data_path="data/builder_train_preprocessed.csv")
+    # print("Standard BKT:", model.evaluate(data_path="data/builder_test_preprocessed.csv", metric="auc"))
     model2 = Model(seed=0, num_fits=20)
     model2.fit(data_path="data/builder_train_preprocessed.csv", forgets=True)
     print("BKT+Forgets:", model2.evaluate(data_path="data/builder_test_preprocessed.csv", metric="auc"))
