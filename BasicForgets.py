9de11239dbc17a9cf7ba622ffcb0258a3e7966fb
Qian <book1925@gmail.com>
2021-10-22 17:38:53 +0800

    update

--- BasicForgets.py
+++ BasicForgets.py
@@ -8,9 +8,9 @@ sys.path.append('../')
 
 def main():
 
-    # model = Model(seed=0, num_fits=20)
-    # model.fit(data_path="data/builder_train_preprocessed.csv")
-    # print("Standard BKT:", model.evaluate(data_path="data/builder_test_preprocessed.csv", metric="auc"))
+    model = Model(seed=0, num_fits=20)
+    model.fit(data_path="data/builder_train_preprocessed.csv")
+    print("Standard BKT:", model.evaluate(data_path="data/builder_test_preprocessed.csv", metric="auc"))
     model2 = Model(seed=0, num_fits=20)
     model2.fit(data_path="data/builder_train_preprocessed.csv", forgets=True)
     print("BKT+Forgets:", model2.evaluate(data_path="data/builder_test_preprocessed.csv", metric="auc"))
--- MyTest.py
+++ MyTest.py
@@ -1,7 +1,9 @@
 import sys
 import numpy as np
-np.seterr(divide='ignore', invalid='ignore')
 from pyBKT.models import Model
+import pandas as pd
+
+np.seterr(divide='ignore', invalid='ignore')
 
 sys.path.append('../')
 
@@ -10,11 +12,22 @@ def main():
     # Initialize the model with an optional seed
     model = Model(seed=42, num_fits=1)
     model.fit(data_path='data/ct.csv', skills=["Plot imperfect radical",
-                                          "Plot pi"],
+                                               "Plot pi"],
               multigs=True, forgets=True,
               multilearn=True)
 
     print(model.params())
 
+
 if __name__ == '__main__':
-    sys.exit(main())
+    # sys.exit(main())
+
+    ct_df = pd.read_csv('data/ct.csv', encoding='latin')
+    print(ct_df.columns)
+    # ct_df.head(5)
+
+    model = Model(seed=42, num_fits=1)
+    model.fit(data_path='data/ct.csv', skills=".*fraction.*")
+    preds = model.predict(data_path='data/ct.csv')
+    preds[['Anon Student Id', 'KC(Default)', 'Correct First Attempt',
+           'correct_predictions', 'state_predictions']].head(5)
