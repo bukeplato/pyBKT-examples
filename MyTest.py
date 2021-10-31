920f27e56b46d21a28e1a6c78fd90c21fce2342c
Qian <book1925@gmail.com>
2021-10-31 11:42:12 +0800

    Update MyTest.py

--- MyTest.py
+++ MyTest.py
@@ -3,6 +3,11 @@ import numpy as np
 from pyBKT.models import Model
 import pandas as pd
 
+pd.set_option('display.max_rows', 50)  #最大行数
+pd.set_option('display.max_columns', None)    #最大列数
+pd.set_option('display.width', None)        #页面宽度
+
+
 np.seterr(divide='ignore', invalid='ignore')
 
 sys.path.append('../')
@@ -10,24 +15,25 @@ sys.path.append('../')
 
 def main():
     # Initialize the model with an optional seed
-    model = Model(seed=42, num_fits=1)
-    model.fit(data_path='data/ct.csv', skills=["Plot imperfect radical",
-                                               "Plot pi"],
-              multigs=True, forgets=True,
-              multilearn=True)
+    # model = Model(seed=42, num_fits=1)
+    # model.fit(data_path='data/ct.csv', skills=["Plot imperfect radical",
+    #                                            "Plot pi"],
+    #           multigs=True, forgets=True,
+    #           multilearn=True)
+    #
+    # print(model.params())
 
-    print(model.params())
-
-
-if __name__ == '__main__':
-    # sys.exit(main())
-
-    ct_df = pd.read_csv('data/ct.csv', encoding='latin')
-    print(ct_df.columns)
+    # ct_df = pd.read_csv('data/ct.csv', encoding='latin')
+    # print(ct_df.columns)
     # ct_df.head(5)
 
     model = Model(seed=42, num_fits=1)
     model.fit(data_path='data/ct.csv', skills=".*fraction.*")
     preds = model.predict(data_path='data/ct.csv')
-    preds[['Anon Student Id', 'KC(Default)', 'Correct First Attempt',
-           'correct_predictions', 'state_predictions']].head(5)
+    print(preds)
+    # print(preds[['Anon Student Id', 'KC(Default)', 'Correct First Attempt',
+    #              'correct_predictions', 'state_predictions']])
+
+
+if __name__ == '__main__':
+    sys.exit(main())
